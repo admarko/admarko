047a48dc6743947e4475d14f537dc87fc836371b
Alex Markowitz <Alex@alexs-mbp-3.lan>
2020-02-20 21:50:23 -0500

    routing set up and functional

--- admarko.xyz/README.md
+++ admarko.xyz/README.md
@@ -66,3 +66,9 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+TODO:
+
+- convert to typescript
+- some git hooks
+- fix directory structure: make components folder, change to **styles** just look at general react styleguide
--- admarko.xyz/src/App.js
+++ admarko.xyz/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/App.css";
+import Home from "./Home";
+import Header from "./Header";
+import Footer from "./Footer";
+import Notes from "./Notes";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/notes" component={Notes} />
+          </Switch>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
-
-export default App;
